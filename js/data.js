86a433443e246e1ca7b26457e91a4b4bdbc58ade
vladconq <vladconq@gmail.com>
2020-05-17 01:14:30 +0300

    footer

--- js/data.js
+++ js/data.js
@@ -20,125 +20,137 @@ quizData = [
     {
         question: 'What is the correct HTML tag for the largest heading?',
         answers: ['<heading>', '<h6>', '<head>', '<h1>'],
-        bingo: 3
+        bingo: '<h1>'
     },
-    // {
-    //     question: 'What is the correct HTML tag for inserting a line break?',
-    //     answers: ['<br>', '<lb>', '<break>'],
-    //     bingo: 0
-    // },
-    // {
-    //     question: 'What is the correct HTML for creating a hyperlink?',
-    //     answers: [
-    //         '<a url="http://www.github.com">Github.com</a>',
-    //         '<a name="http://www.github.com">Github.com</a>',
-    //         '<a href="http://www.github.com">Github.com</a>',
-    //         '<a>http://www.github.com</a>'
-    //     ],
-    //     bingo: 2
-    // },
-    // {
-    //     question: 'What is the correct HTML for inserting an image?',
-    //     answers: [
-    //         '<img alt="MyImage">image.gif</img>',
-    //         '<img src="image.gif" alt="MyImage">',
-    //         '<image src="image.gif" alt="MyImage">',
-    //         '<img href="image.gif" alt="MyImage">'
-    //     ],
-    //     bingo: 1
-    // },
-    // {
-    //     question: 'How can you make a numbered list?',
-    //     answers: [
-    //         '<ol>',
-    //         '<ul>',
-    //         '<dl>',
-    //         '<list>'
-    //     ],
-    //     bingo: 0
-    // },
-    // {
-    //     question: 'What is the correct HTML for making a checkbox?',
-    //     answers: [
-    //         '<checkbox>',
-    //         '<input type="checkbox">',
-    //         '<check>',
-    //         '<input type="check">'
-    //     ],
-    //     bingo: 1
-    // },
-    // {
-    //     question: 'Which sign is a valid identifier of an alias for jQuery?',
-    //     answers: [
-    //         'the * sign',
-    //         'the ! sign',
-    //         'the # sign',
-    //         'the $ sign'
-    //     ],
-    //     bingo: 3
-    // },
-    // {
-    //     question: 'What does $("div") select?',
-    //     answers: [
-    //         'All div elements',
-    //         'The first div element',
-    //         'An element with id "div"', 
-    //         'An element with class "div"'
-    //     ],
-    //     bingo: 0
-    // },
-    // {
-    //     question: 'What is the correct jQuery code to set the background color of all p elements to red?',
-    //     answers: [
-    //         '$("p").css("background-color","red");',
-    //         '$("p").manipulate("background-color","red");',
-    //         '$("p").layout("background-color","red");',
-    //         '$("p").style("background-color","red");'
-    //     ],
-    //     bingo: 0
-    // },
-    // {
-    //     question: 'What does $("div.intro") select?',
-    //     answers: [
-    //         'All div elements with id="intro"',
-    //         'The first div element with id="intro"',
-    //         'The first div element with class="intro"',
-    //         'All div elements with class="intro"'
-    //     ],
-    //     bingo: 3
-    // },
-    // {
-    //     question: 'Which jQuery method is used to hide selected elements?',
-    //     answers: [
-    //         'hidden()',
-    //         'visible(false)',
-    //         'hide()',
-    //         'display(none)'
-    //     ],
-    //     bingo: 2
-    // },
-    // {
-    //     question: 'What is the newest version of HTML being developed?',
-    //     answers: [
-    //         'HTML3',
-    //         'HTML4',
-    //         'HTML5',
-    //         'There is only one type of HTML'
-    //     ],
-    //     bingo: 2
-    // },
+    {
+        question: 'What is the correct HTML tag for inserting a line break?',
+        answers: ['<br>', '<lb>', '<break>'],
+        bingo: '<br>'
+    },
+    {
+        question: 'What is the correct HTML for creating a hyperlink?',
+        answers: [
+            '<a url="http://www.github.com">Github.com</a>',
+            '<a name="http://www.github.com">Github.com</a>',
+            '<a href="http://www.github.com">Github.com</a>',
+            '<a>http://www.github.com</a>'
+        ],
+        bingo: '<a href="http://www.github.com">Github.com</a>'
+    },
+    {
+        question: 'What is the correct HTML for inserting an image?',
+        answers: [
+            '<img alt="MyImage">image.gif</img>',
+            '<img src="image.gif" alt="MyImage">',
+            '<image src="image.gif" alt="MyImage">',
+            '<img href="image.gif" alt="MyImage">'
+        ],
+        bingo: '<img src="image.gif" alt="MyImage">'
+    },
+    {
+        question: 'How can you make a numbered list?',
+        answers: [
+            '<ol>',
+            '<ul>',
+            '<dl>',
+            '<list>'
+        ],
+        bingo: '<ol>'
+    },
+    {
+        question: 'What is the correct HTML for making a checkbox?',
+        answers: [
+            '<checkbox>',
+            '<input type="checkbox">',
+            '<check>',
+            '<input type="check">'
+        ],
+        bingo: '<input type="checkbox">'
+    },
+    {
+        question: 'Which sign is a valid identifier of an alias for jQuery?',
+        answers: [
+            'the * sign',
+            'the ! sign',
+            'the # sign',
+            'the $ sign'
+        ],
+        bingo: 'the $ sign'
+    },
+    {
+        question: 'What does $("div") select?',
+        answers: [
+            'All div elements',
+            'The first div element',
+            'An element with id "div"', 
+            'An element with class "div"'
+        ],
+        bingo: 'All div elements'
+    },
+    {
+        question: 'What is the correct jQuery code to set the background color of all p elements to red?',
+        answers: [
+            '$("p").css("background-color","red");',
+            '$("p").manipulate("background-color","red");',
+            '$("p").layout("background-color","red");',
+            '$("p").style("background-color","red");'
+        ],
+        bingo: '$("p").css("background-color","red");'
+    },
+    {
+        question: 'What does $("div.intro") select?',
+        answers: [
+            'All div elements with id="intro"',
+            'The first div element with id="intro"',
+            'The first div element with class="intro"',
+            'All div elements with class="intro"'
+        ],
+        bingo: 'All div elements with class="intro"'
+    },
+    {
+        question: 'Which jQuery method is used to hide selected elements?',
+        answers: [
+            'hidden()',
+            'visible(false)',
+            'hide()',
+            'display(none)'
+        ],
+        bingo: 'hide()'
+    },
+    {
+        question: 'What is the newest version of HTML being developed?',
+        answers: [
+            'HTML3',
+            'HTML4',
+            'HTML5',
+            'There is only one type of HTML'
+        ],
+        bingo: 'HTML5'
+    },
     {
         question: 'Are CSS property names case-sensitive?',
         answers: [
             'Yes',
             'No'
         ],
-        bingo: 1,
+        bingo: 'No',
         code: (
-        `ul {
-        \t MaRGin: 10px; 
-        }`).replace(/  +/g, ''),
-        language: 'language-css'
+            `ul {
+        \tMaRGin: 10px; 
+        }`).replace(/  +/g, '')
+    },
+    {
+        question: 'What will the code above print to the console?',
+        code: (
+            `var a = 5;
+            a++;
+            a -= 3;
+            a *= 2;
+            a--;
+            console.log(a);`
+        ).replace(/  +/g, ''),
+        answers: ['3', '4', '5', '6'],
+        bingo: '5'
     }
 ]
 
--- js/script.js
+++ js/script.js
@@ -1,28 +1,12 @@
-// $('.jumbotron .btn').on('click', function () {
-//     $('.jumbotron').removeClass('success fail')
-//     $('.jumbotron p').text('Learning is never too late. Test your knowledge.')
-//     $('#answers').empty()
-
-//     codeDisplay = document.querySelector('code')
-//     codeDisplay.classList.remove(language)
-//     codeDisplay.textContent = ''
-
-//     console.log(codeDisplay)
-
-
-
-//     renderQuizCard()
-//     pickCard()
-// })
-
 function renderQuizCard(question, code, language, answers) {
     // render question
     $('#question').text(question)
 
     // render code if exists
     if (code) {
+        $('code').show()
         $('code').text(code)
-        $('code').addClass(language)
+        hljs.highlightBlock(document.querySelector('code'));
     }
 
     // render answers
@@ -65,14 +49,11 @@ function pickCard(bingo) {
             setTimeout(function () {
                 $('.jumbotron p').removeClass("hide")
             }, 200)
-            // $('.jumbotron p').animate({'opacity': 0}, 200, function() {
-            //     $('.jumbotron p').text(failText[Math.floor(Math.random() * failText.length)])
-            // }).animate({'opacity': 1}, 0)
         }
     })
 }
 
-function getRandomQuizCard() {
+function newQuizCard() {
     let randomCard = quizData[Math.floor(Math.random() * quizData.length)]
     let question = randomCard['question']
     let code = randomCard['code']
@@ -84,131 +65,15 @@ function getRandomQuizCard() {
     pickCard(bingo)
 }
 
-// function renderQuizCard() {
-    
+function randomButton() {
+    $('.jumbotron button').on('click', function () {
+        $('.jumbotron').removeClass('success fail')
+        $('.jumbotron p').text('Learning is never too late. Test your knowledge.')
+        $('#answers').empty()
+        $('code').hide()
+        newQuizCard()
+    })
+}
 
-    // $('#answers').add(answers[0])
-    // $('#answers').addClass('btn')
-    // $('#answers').each(function(index) {
-    //     console.log(index)
-    //     $(this).text(answers[index])
-    // })
-
-
-    // questionDisplay = document.querySelector('#question')
-    // answersDisplay = document.querySelector('#answers')
-    // codeDisplay = document.querySelector('code')
-
-    // // render question
-    // questionDisplay.textContent = question
-
-    // codeDisplay.classList.add(language)
-    // codeDisplay.textContent = code
-
-    // console.log(codeDisplay)
-
-    // // render answers
-    // answers.forEach(answer => {
-    //     newAnswerDisplay = document.createElement('button')
-    //     newAnswerDisplay.textContent = answer
-    //     newAnswerDisplay.classList.add("btn", "shadow-none")
-    //     answersDisplay.appendChild(newAnswerDisplay)
-    // })
-// }
-
-// function pickCard() {
-//     let randomCard = getRandomQuizCard()
-//     let bingo = randomCard['bingo']
-
-//     // checking the selected answer
-//     $('#answers button').on('click', function () {
-//         if ($(this).text() === bingo) {
-//             $(this).addClass('bingo')
-//             $('.jumbotron').addClass('success')
-//             // disable functionallity of buttons
-//             $('#answers .btn').off()
-//             // disable in design side
-//             $('#answers .btn').not('.bingo').addClass('disabled')
-
-//             $('.jumbotron p').addClass("hide")
-//             setTimeout(function () {
-//                 $('.jumbotron p').text(successText[Math.floor(Math.random() * failText.length)])
-//             }, 200)
-//             setTimeout(function () {
-//                 $('.jumbotron p').removeClass("hide")
-//             }, 200)
-//         }
-//         else {
-//             $(this).addClass('non-bingo')
-//             $('.jumbotron').addClass('fail')
-
-//             $('.jumbotron p').addClass("hide")
-//             setTimeout(function () {
-//                 $('.jumbotron p').text(failText[Math.floor(Math.random() * failText.length)])
-//             }, 200)
-//             setTimeout(function () {
-//                 $('.jumbotron p').removeClass("hide")
-//             }, 200)
-//             // $('.jumbotron p').animate({'opacity': 0}, 200, function() {
-//             //     $('.jumbotron p').text(failText[Math.floor(Math.random() * failText.length)])
-//             // }).animate({'opacity': 1}, 0)
-//         }
-//     })
-// }
-
-// function pickCard() {
-//     answersButtonsDisplay = document.querySelectorAll('#answers .btn')
-//     jumbotronDisplay = document.querySelector('.jumbotron')
-//     jumbotronTextDisplay = document.querySelector('.jumbotron p')
-
-//     answersButtonsDisplay.forEach(answerButtonDisplay => {
-//         answerButtonDisplay.addEventListener('click', function () {
-//             if (this.textContent === bingo) {
-//                 this.style.backgroundColor = '#6ab04c'
-//                 this.style.color = 'white'
-//                 jumbotronDisplay.classList.add("success")
-//                 jumbotronTextDisplay.classList.add("hide")
-//                 setTimeout(function () {
-//                     jumbotronTextDisplay.textContent = successText[Math.floor(Math.random() * successText.length)]
-//                 }, 200)
-//                 setTimeout(function () {
-//                     jumbotronTextDisplay.classList.remove('hide')
-//                 }, 200)
-//             }
-//             else {
-//                 this.style.backgroundColor = '#eb4d4b'
-//                 this.style.color = 'white'
-//                 jumbotronDisplay.classList.add("fail")
-//                 jumbotronTextDisplay.classList.add("hide")
-//                 setTimeout(function () {
-//                     jumbotronTextDisplay.textContent = failText[Math.floor(Math.random() * failText.length)]
-//                 }, 200)
-//                 setTimeout(function () {
-//                     jumbotronTextDisplay.classList.remove('hide')
-//                 }, 200)
-//                 this.classList.add('disabled')
-//             }
-//         })
-//     })
-// }
-
-renderQuizCard()
-pickCard()
-
-
-// answerBtns = document.querySelectorAll('#answers .btn')
-// answerBtns.forEach(answerBtn => {
-//     answerBtn.addEventListener('click', function() {
-//         if(this.textContent === bingo) {
-//             this.style.backgroundColor = '#6ab04c'
-//             this.style.color = 'white'
-//             jumbotron.classList.add("success")
-//         }
-//         else {
-//             this.style.backgroundColor = '#eb4d4b'
-//             this.style.color = 'white'
-//             jumbotron.classList.add("fail")
-//         }
-//     })
-// })
-
+randomButton()
+newQuizCard()
